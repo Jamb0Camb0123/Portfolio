399a4c4665b37b0bd9e9fec97d1ff665fdc26e2e
Jamb0Camb0123 <jamie.b.campbell@outlook.com>
2025-03-03 14:07:26 +0000

    Including 3 Site pages Including Header and Footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <p>&copy; {new Date().getFullYear()} Your Portfolio. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>Hello! I'm [Your Name], a [Your Profession] based in [Your Location]. I have a passion for [Your Interests or Skills].</p>
+      <p>With [X] years of experience in [Your Field], I have worked on various projects involving [Technologies or Areas of Expertise].</p>
+      <p>In my free time, I enjoy [Your Hobbies or Activities].</p>
+    </div>
+  );
+};
+
+export default About;
