b6edb259c32f8781712744651b50b1846f4ee674
Jamb0Camb0123 <jamie.b.campbell@outlook.com>
2025-03-03 14:39:42 +0000

    Changes to Site Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header.tsx";
-import Footer from "./components/Footer.tsx";
-import Home from "./pages/Home.tsx";
-import About from "./pages/About.tsx";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 function App() {
@@ -10,9 +10,9 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio/home" element={<Home />} />
+        <Route path="/portfolio/about" element={<About />} />
+        <Route path="/portfolio/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
